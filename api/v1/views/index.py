643b482ceb2892fb2e2c27705705012e48cc9b3c
ca-m <deconnick.carmel@gmail.com>
2023-10-02 23:12:35 +0200

    update github files from ubuntu

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,12 @@
-#!/usr/bin/python3xx
+#!/usr/bin/python3
 '''api status'''
-import models
 from models import storage
-from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 from flask import jsonify
 from api.v1.views import app_views
 
@@ -16,9 +20,11 @@ def returnstuff():
 @app_views.route('/stats', strict_slashes=False)
 def stuff():
 	'''JSON Responses'''
-	todos = {'states': State, 'users': User,
-			'amenities': Amenity, 'cities': City,
-			'places': Place, 'reviews': Review}
+	todos = {
+		'states': State, 'users': User,
+		'amenities': Amenity, 'cities': City,
+		'places': Place, 'reviews': Review
+	}
 	for key in todos:
 		todos[key] = storage.count(todos[key])
 	return jsonify(todos)
